adae24ea8a1bcfbc85e7ba1af414584a53851c55
Vitaly <vasilvvmain@gmail.com>
2023-05-02 18:00:09 +0200

    Added Activity typescript model

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -1,23 +1,24 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import axios from "axios";
 import { Button, Header, List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
 
 function App() {
-  const [activities, setAtivities] = useState([]);
+  const [activities, setAtivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/activities").then((response) => {
-      setAtivities(response.data);
-    });
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setAtivities(response.data);
+      });
   }, []);
 
   return (
     <div>
       <Header as="h2" icon="users" content="Reactivities" />
       <List>
-        {activities.map((activity: any) => (
+        {activities.map((activity) => (
           <List.Item key={activity.id}>{activity.title}</List.Item>
         ))}
       </List>
