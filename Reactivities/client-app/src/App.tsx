07e09d6baec28d309c76baff0b85042a8c76ef5f
Vitaly <vasilvvmain@gmail.com>
2023-05-02 15:23:43 +0200

    Added prettier for client app, added sample api data loading

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -1,31 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Button, Header, List } from "semantic-ui-react";
 
 function App() {
   const [activities, setAtivities] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities')
-      .then(response => {
-        console.log(response);
-        setAtivities(response.data);
-      });
+    axios.get("http://localhost:5000/api/activities").then((response) => {
+      setAtivities(response.data);
+    });
   }, []);
 
   return (
-    <div className="App">
+    <div>
       <Header as="h2" icon="users" content="Reactivities" />
-        <img src={logo} className="App-logo" alt="logo" />
-        <List>
-          {activities.map((activity : any) => (
-            <li key={activity.id}>
-              {activity.title}
-            </li>
-          ))}
-        </List>
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+      <Button content="test" />
     </div>
   );
 }
