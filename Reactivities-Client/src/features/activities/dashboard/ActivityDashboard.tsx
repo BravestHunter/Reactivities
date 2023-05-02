7f12a6c11f73f95290f8793bfa142dfaf7aaa84d
Vitaly <vasilvvmain@gmail.com>
2023-05-02 19:22:31 +0200

    Added ActivityDetails card component

--- Reactivities-Client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities-Client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, List } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
 
 interface Props {
   activities: Activity[];
@@ -12,6 +13,11 @@ export default function ActivityDashboard(props: Props) {
     <Grid>
       <Grid.Column width="10">
         <ActivityList activities={props.activities} />
+      </Grid.Column>
+      <Grid.Column width="6">
+        {props.activities[0] && (
+          <ActivityDetails activity={props.activities[0]} />
+        )}
       </Grid.Column>
     </Grid>
   );
---
+++ Reactivities-Client/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Activity } from "../../../app/models/activity";
+import { Button, Card, Icon, Image } from "semantic-ui-react";
+
+interface Props {
+  activity: Activity;
+}
+
+export default function ActivityDetails(props: Props) {
+  return (
+    <Card fluid>
+      <Image
+        src={`/assets/categoryImages/${props.activity.category}.jpg`}
+        wrapped
+        ui={false}
+      />
+      <Card.Content>
+        <Card.Header>Matthew</Card.Header>
+        <Card.Meta>
+          <span>{props.activity.date}</span>
+        </Card.Meta>
+        <Card.Description>{props.activity.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group widths="2">
+          <Button basic color="blue" content="Edit" />
+          <Button basic color="grey" content="cancel" />
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  );
+}
