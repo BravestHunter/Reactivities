c10dad6512d6c0dae03b52d40041b4621720ec9c
Vitaly <vasilvvmain@gmail.com>
2023-05-05 18:43:17 +0200

    Added simple fluent model validation in Api app

--- Reactivities-API/API/Extensions/ApplicationServiceExtensions.cs
+++ Reactivities-API/API/Extensions/ApplicationServiceExtensions.cs
@@ -3,6 +3,8 @@ using Application.Core;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using FluentValidation.AspNetCore;
+using FluentValidation;
 
 namespace API.Extensions
 {
@@ -17,6 +19,8 @@ namespace API.Extensions
             });
             services.AddMediatR(typeof(List.Handler));
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssemblyContaining<Create>();
 
             services.AddControllers();
             services.AddCors(opt =>
