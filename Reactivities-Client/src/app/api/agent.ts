14abd08ac0c977902dcaed1e3a00e71a24a7b0d7
Vitaly <vasilvvmain@gmail.com>
2023-05-05 20:28:36 +0200

    Added test 'BuggyApi', added error notification in Client App

--- Reactivities-Client/src/app/api/agent.ts
+++ Reactivities-Client/src/app/api/agent.ts
@@ -1,20 +1,45 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import { Activity } from "../models/activity";
+import { toast } from "react-toastify";
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
     setTimeout(resolve, delay);
   });
 };
-axios.interceptors.response.use(async (response) => {
-  try {
+axios.interceptors.response.use(
+  async (response) => {
     await sleep(1000);
     return response;
-  } catch (error) {
-    console.log(error);
-    return await Promise.reject(error);
+  },
+  (error: AxiosError) => {
+    const { data, status } = error.response!;
+
+    switch (status) {
+      case 400:
+        toast.error("Bad request");
+        break;
+
+      case 401:
+        toast.error("Unauthorized");
+        break;
+
+      case 403:
+        toast.error("Forbidden");
+        break;
+
+      case 404:
+        toast.error("NotFound");
+        break;
+
+      case 500:
+        toast.error("Server error");
+        break;
+    }
+
+    return Promise.reject(error);
   }
-});
+);
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
