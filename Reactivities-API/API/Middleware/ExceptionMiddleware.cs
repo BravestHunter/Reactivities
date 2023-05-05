53bd646679cc4e33c7b0ff5229d7a8bc39c7b124
Vitaly <vasilvvmain@gmail.com>
2023-05-05 20:11:09 +0200

    Added exception handling middleware in Api App

---
+++ Reactivities-API/API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,56 @@
+using System.Net;
+using System.Text.Json;
+using Application.Core;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _hostEnvironment;
+
+        public ExceptionMiddleware(
+            RequestDelegate next,
+            ILogger<ExceptionMiddleware> logger,
+            IHostEnvironment hostEnvironment
+            )
+        {
+            this._next = next;
+            this._logger = logger;
+            this._hostEnvironment = hostEnvironment;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                AppException response;
+                if (_hostEnvironment.IsDevelopment())
+                {
+                    response = new AppException(
+                        context.Response.StatusCode, ex.Message, ex.StackTrace.ToString()
+                        );
+                }
+                else
+                {
+                    response = new AppException(context.Response.StatusCode, "Internal server error");
+                }
+
+                var options = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                var jsonResponse = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(jsonResponse);
+            }
+        }
+    }
+}
