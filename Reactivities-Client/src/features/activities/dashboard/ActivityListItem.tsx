0edb2a379c786723459f08c360f6ae66f3d17fc8
Vitaly <vasilvvmain@gmail.com>
2023-05-05 15:32:19 +0200

    Restyled home page in Client App

--- Reactivities-Client/src/features/activities/dashboard/ActivityListItem.tsx
+++ Reactivities-Client/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,8 +1,6 @@
-import { SyntheticEvent, useState } from "react";
 import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { Link } from "react-router-dom";
-import { useStore } from "../../../app/stores/store";
 
 interface Props {
   activity: Activity;
@@ -10,19 +8,6 @@ interface Props {
 
 export default function ActivityListItem(props: Props) {
   const { activity } = props;
-
-  const { activityStore } = useStore();
-  const { deleteActivity, loading } = activityStore;
-
-  const [target, setTarget] = useState("");
-
-  function hundleActivityDelete(
-    e: SyntheticEvent<HTMLButtonElement>,
-    id: string
-  ) {
-    setTarget(e.currentTarget.name);
-    deleteActivity(id);
-  }
 
   return (
     <Segment.Group>
