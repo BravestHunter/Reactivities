c66a9e57a382bc08d543122d1e407869ef4f3d94
Vitaly <vasilvvmain@gmail.com>
2023-05-15 17:29:11 +0200

    Added scroll restoration in Client App

--- Reactivities-Client/src/app/layout/App.tsx
+++ Reactivities-Client/src/app/layout/App.tsx
@@ -2,7 +2,7 @@ import { Fragment, useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import { observer } from "mobx-react-lite";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, ScrollRestoration, useLocation } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import { ToastContainer } from "react-toastify";
 import { useStore } from "../stores/store";
@@ -26,7 +26,8 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <>
+      <ScrollRestoration />
       <ModalContainer />
       <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
 
@@ -40,7 +41,7 @@ function App() {
           </Container>
         </>
       )}
-    </Fragment>
+    </>
   );
 }
 
