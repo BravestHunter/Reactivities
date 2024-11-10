bc767736fbafdd0fcea58e68d0feb5fbd3257f5b
Vitaly <vasilvvmain@gmail.com>
2024-11-10 05:10:37 +0100

    Made enpoints from BuggyController dev only

---
+++ Reactivities-API/Reactivities.Api/Attributes/DevOnlyAttribute.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Reactivities.Api.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class DevOnlyAttribute : Attribute, IFilterFactory
+    {
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+        {
+            return new DevOnlyAttributeImpl(serviceProvider.GetRequiredService<IWebHostEnvironment>());
+        }
+
+        public bool IsReusable => true;
+
+        [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+        private sealed class DevOnlyAttributeImpl : Attribute, IAuthorizationFilter
+        {
+            public DevOnlyAttributeImpl(IWebHostEnvironment hostingEnv)
+            {
+                HostingEnv = hostingEnv;
+            }
+
+            private IWebHostEnvironment HostingEnv { get; }
+
+            public void OnAuthorization(AuthorizationFilterContext context)
+            {
+                if (!HostingEnv.IsDevelopment())
+                {
+                    context.Result = new NotFoundResult();
+                }
+            }
+        }
+    }
+}
--- Reactivities-API/Reactivities.Api/Controllers/BuggyController.cs
+++ Reactivities-API/Reactivities.Api/Controllers/BuggyController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Reactivities.Api.Attributes;
 using Reactivities.Api.Exceptions;
 
 namespace Reactivities.Api.Controllers
@@ -6,24 +7,28 @@ namespace Reactivities.Api.Controllers
     public class BuggyController : ControllerBase
     {
         [HttpGet("not-found")]
+        [DevOnly]
         public ActionResult GetNotFound()
         {
             return NotFound();
         }
 
         [HttpGet("bad-request")]
+        [DevOnly]
         public ActionResult GetBadRequest()
         {
             return BadRequest("This is a bad request");
         }
 
         [HttpGet("server-error")]
+        [DevOnly]
         public ActionResult GetServerError()
         {
             throw new SampleException();
         }
 
         [HttpGet("unauthorised")]
+        [DevOnly]
         public ActionResult GetUnauthorised()
         {
             return Unauthorized();
