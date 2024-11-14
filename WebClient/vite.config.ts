358d43841491be1c2bc2e9176f518b05b43b079a
Vitaly <vasilvvmain@gmail.com>
2024-11-14 09:52:13 +0100

    Added scripts to run and develop WebClient app in docker container

--- WebClient/vite.config.ts
+++ WebClient/vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000
+    host: '0.0.0.0',
+    port: 3000,
+    hmr: {
+      port: 3001
+    }
   },
   plugins: [react()],
 })
