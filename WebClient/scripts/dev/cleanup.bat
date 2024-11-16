eb885d9b163bbec92523e385a9ebca22f392c269
Vitaly <vasilvvmain@gmail.com>
2024-11-16 16:34:45 +0100

    Added mapping of docker node_modules volume to local project directory to enable vs code entellisense in WebClient

--- WebClient/scripts/dev/cleanup.bat
+++ WebClient/scripts/dev/cleanup.bat
@@ -1,1 +1,8 @@
 docker volume rm reactivities-nodemodules
+
+set NODE_MODULES_PATH=%~dp0\..\..\node_modules
+
+if exist "%NODE_MODULES_PATH%" (
+    echo Deleting directory: %NODE_MODULES_PATH%
+    rmdir /s /q "%NODE_MODULES_PATH%"
+)
--- WebClient/scripts/dev/setup.bat
+++ WebClient/scripts/dev/setup.bat
@@ -1,1 +1,8 @@
-docker volume create reactivities-nodemodules
+@echo off
+
+docker volume create ^
+  --driver local ^
+  --opt type=none ^
+  --opt o=bind ^
+  --opt device=%~dp0\..\..\node_modules ^
+  reactivities-nodemodules
