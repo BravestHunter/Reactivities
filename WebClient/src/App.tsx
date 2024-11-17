fdd3b1d2c5b174edce6731708adb3bd3323ba08a
Vitaly <vasilvvmain@gmail.com>
2024-11-18 00:05:45 +0100

    Removed unused css files, fixed auth page style

--- WebClient/src/App.tsx
+++ WebClient/src/App.tsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router/Routes'
-import './App.css'
 import { globalStore, GlobalStoreContext } from './app/stores/globalStore'
 
 function App() {
