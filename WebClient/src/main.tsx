20286b6ee84cdacb4d493825fe48fb56bedc9ed6
Vitaly <vasilvvmain@gmail.com>
2024-11-19 03:05:57 +0100

    Added missing react-datepicker style file import

--- WebClient/src/main.tsx
+++ WebClient/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import 'semantic-ui-css/semantic.min.css'
 import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-datepicker/dist/react-datepicker.min.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
