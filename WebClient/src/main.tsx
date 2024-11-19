b99c2b3c12513cd456d7e09298fd998c0d2bc1ff
Vitaly <vasilvvmain@gmail.com>
2024-11-19 23:58:06 +0100

    Added missing react-calendar style file import

--- WebClient/src/main.tsx
+++ WebClient/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import 'semantic-ui-css/semantic.min.css'
 import 'react-toastify/dist/ReactToastify.min.css'
 import 'react-datepicker/dist/react-datepicker.min.css'
+import 'react-calendar/dist/Calendar.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
